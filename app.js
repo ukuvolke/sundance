416e24a6ec9e4d432953a66c77af449b6fe5c949
Uku Volke <ukuvolke@gmail.com>
2021-05-03 19:00:36 +0300

    esketit

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 
 function getData() {
-    // Request data for entered location (default: Winnersh, Berkshire, UK)
-    let lat = document.getElementById("lat").value;
-    let long = document.getElementById("long").value;
-    let date = document.getElementById("date").value;
+  // Request data for entered location (default: Winnersh, Berkshire, UK)
+  let lat = document.getElementById("lat").value;
+  let long = document.getElementById("long").value;
+  let date = document.getElementById("date").value;
 
-  
-    fetch(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${long}&date=${date}&formatted=0`, {
-      // nothing here
-      }).then(response => response.json())
-        .then(showData)
-    //  .catch(e => requestError(e, 'Error'));
-    }
+
+  fetch(`https://api.sunrise-sunset.org/json?lat=${lat}&lng=${long}&date=${date}&formatted=0`, {
+    // nothing here
+  }).then(response => response.json())
+    .then(showData)
+  //  .catch(e => requestError(e, 'Error'));
+}
 
 
 
@@ -20,41 +20,41 @@ var field = document.querySelector('#date');
 var now = new Date();
 var day = ("0" + now.getDate()).slice(-2);
 var month = ("0" + (now.getMonth() + 1)).slice(-2);
-var today = now.getFullYear()+"-"+(month)+"-"+(day);
+var today = now.getFullYear() + "-" + (month) + "-" + (day);
 console.log(today);
-field.value=today;
+field.value = today;
 
 function showData(data) {
 
-    var riseText="The sunrise is schedulded at ";
-    var setText="The sunset is scheduled at ";
-    var lengthText="The length of the day is ";
-    dataitem = data['results'];
-    var localSunrise = new Date(dataitem.sunrise).toLocaleTimeString();
-    var localSunset =  new Date(dataitem.sunset).toLocaleTimeString();
-    var dayLength = new Date(dataitem.day_length * 1000).toISOString().substr(11, 8);
-    var disclaimer = "Data supplied by https://sunrise-sunset.org/";
-    
-    console.log(dayLength);
-    document.getElementById("sunrise").innerHTML= riseText + localSunrise;
-    document.getElementById("sunset").innerHTML= setText + localSunset;
-    document.getElementById("daylength").innerHTML=lengthText + dayLength;
-    document.getElementById("disclaimer").innerHTML=disclaimer;
-    console.log(data);
+  var riseText = "The sunrise is schedulded at ";
+  var setText = "The sunset is scheduled at ";
+  var lengthText = "The length of the day is ";
+  dataitem = data['results'];
+  var localSunrise = new Date(dataitem.sunrise).toLocaleTimeString();
+  var localSunset = new Date(dataitem.sunset).toLocaleTimeString();
+  var dayLength = new Date(dataitem.day_length * 1000).toISOString().substr(11, 8);
+  var disclaimer = "Data supplied by https://sunrise-sunset.org/";
 
-    }
+  console.log(dayLength);
+  document.getElementById("sunrise").innerHTML = riseText + localSunrise;
+  document.getElementById("sunset").innerHTML = setText + localSunset;
+  document.getElementById("daylength").innerHTML = lengthText + dayLength;
+  document.getElementById("disclaimer").innerHTML = disclaimer;
+  console.log(data);
 
-    const localmap = new ol.Map({
-      layers: [
-          new ol.layer.Tile({
-              source: new ol.source.OSM()
-          })
-      ],
-      target: 'map',
-      view: new ol.View({
-          center: [0, 0],
-          zoom: 2
-      })
+}
+
+const localmap = new ol.Map({
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    })
+  ],
+  target: 'map',
+  view: new ol.View({
+    center: [0, 22],
+    zoom: 4
+  })
 });
 localmap.on('singleclick', function (evt) {
 
@@ -62,14 +62,14 @@ localmap.on('singleclick', function (evt) {
   var lonlat = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
   var lon = lonlat[0];
   var lat = lonlat[1];
-  var lonField=document.querySelector("#long");
-  var latField=document.querySelector("#lat");
-  lonField.value=lon;
-  latField.value=lat;
+  var lonField = document.querySelector("#long");
+  var latField = document.querySelector("#lat");
+  lonField.value = lon;
+  latField.value = lat;
 
 
-  
-  
+
+
 });
 
 
@@ -82,10 +82,10 @@ var options = {
 function success(pos) {
   var crd = pos.coords;
 
-  var lonField=document.querySelector("#long");
-  var latField=document.querySelector("#lat");
-  lonField.value=crd.latitude;
-  latField.value=crd.longitude;
+  var lonField = document.querySelector("#long");
+  var latField = document.querySelector("#lat");
+  lonField.value = crd.longitude;
+  latField.value = crd.latitude;
 
   console.log('Your current position is:');
   console.log(`Latitude : ${crd.latitude}`);
